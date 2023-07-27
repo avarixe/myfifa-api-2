8952b270a35eb448d0416f336d4beef8fa75cd41
Joon Lee <l33j00n325@gmail.com>
2023-07-27 18:29:31 +0000

    add sequelize

--- index.ts
+++ index.ts
@@ -1,11 +1,10 @@
 import express, { Express, Request, Response } from 'express'
 import morgan from 'morgan'
-import dotenv from 'dotenv'
+import 'dotenv/config'
 
 import { createYoga } from 'graphql-yoga'
-import { schema } from './schema'
-
-dotenv.config();
+import schema from './schema'
+import database from './database'
 
 const yoga = createYoga({ schema })
 
@@ -17,9 +16,16 @@ app.use(morgan('combined'));
 app.use(yoga.graphqlEndpoint, yoga)
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + sdfg Server');
+  res.redirect('/graphql')
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  try {
+    await database.authenticate()
+    console.log('Connection has been established successfully')
+  } catch (err) {
+    console.error('Unable to connect to the database:', err)
+  }
+
   console.log(`[server]: Running a GraphQL API server at http://localhost:${port}/graphql`);
 });
