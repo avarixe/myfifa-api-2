257d2789aa44a2e78762bff74a0b2db4ca257c23
Joon Lee <l33j00n325@gmail.com>
2023-07-27 17:17:51 +0000

    add typescript

--- index.ts
+++ index.ts
@@ -2,17 +2,24 @@ import express, { Express, Request, Response } from 'express'
 import morgan from 'morgan'
 import dotenv from 'dotenv'
 
+import { createYoga } from 'graphql-yoga'
+import { schema } from './schema'
+
 dotenv.config();
+
+const yoga = createYoga({ schema })
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('combined'));
 
+app.use(yoga.graphqlEndpoint, yoga)
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + sdfg Server');
 });
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`[server]: Running a GraphQL API server at http://localhost:${port}/graphql`);
 });
